c3870d5cf564c638c32902a5c0a2e21cd3c06d1d
Dinushkasam <dinushkasam@gmail.com>
2023-10-14 17:51:22 +0530

    bus route search

--- server/controllers/busRouteController.js
+++ server/controllers/busRouteController.js
@@ -15,3 +15,43 @@ export const createBusRoute = asyncHandler(async (req, res) => {
         res.status(405).json({ message: error.message });
     }
 });
+
+export const getBusRoutesBySearch = asyncHandler(async (req, res) => {
+    const { term } = req.params;
+    try {
+        const busRoutes = await BusRoute.aggregate([
+            {
+                $match: {
+                    $or: [
+                        { routeNumber: { $regex: term, $options: "i" } },
+                        { routeName: { $regex: term, $options: "i" } },
+                    ],
+                },
+            },
+            {
+                $lookup: {
+                    from: "busstops",
+                    localField: "stops",
+                    foreignField: "_id",
+                    as: "stops",
+                },
+            },
+            {
+                $project: {
+                    _id: 1,
+                    routeNumber: 1,
+                    routeName: 1,
+                    stops: {
+                        _id: 1,
+                        name: 1,
+                    },
+                    fareRate: 1,
+                },
+            },
+        ]).hint({ routeNumber: 1, routeName: 1 });
+
+        res.status(200).json(busRoutes);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
