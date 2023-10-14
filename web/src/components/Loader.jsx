1c6a40881f822572f601fce930cd4bc633414567
Navod <54998118+navodveduth@users.noreply.github.com>
2023-10-14 23:32:46 +0530

    minor style changes

--- web/src/components/Loader.jsx
+++ web/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { FaSpinner } from 'react-icons/fa';
 const Loader = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <FaSpinner className="animate-spin text-2xl text-white" />
+            <FaSpinner className="animate-spin text-2xl text-black" />
             <span className="ml-4 text-primarylight">Loading...</span>
         </div>
     );
--- web/src/pages/Dashboard.jsx
+++ web/src/pages/Dashboard.jsx
@@ -9,7 +9,6 @@ const Dashboard = () => {
     const [loading, setLoading] = useState(false);
 
     // const fetchSite = async () => {
-    //     // const visits = await getSiteVisits(user.token);
     //     setLoading(false);
     // };
 
@@ -33,16 +32,9 @@ const Dashboard = () => {
             {isAccess && loading ? (
                 <Loader />
             ) : (
-                <>
-                    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-8 mt-8 px-4 md:px-10">
-                        <div className="w-full">
-                            DashBoard
-                            {/* <BoxWidget heading={'Traffic'} value={formatVisits} icon={'fa-solid fa-arrow-trend-up'} /> */}
-                        </div>
-                    </div>
-
-                    {/* <LineChart siteVisits={siteVisits} loading={loading} /> */}
-                </>
+                <div className="grid gap-4 md:gap-8 mt-8 pb-10 md:px-5 bg-white rounded-xl overflow-x-auto">
+                    DashBoard
+                </div>
             )}
         </>
     );
