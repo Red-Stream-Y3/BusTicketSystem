d907bab08a1fe881d87816bb4498a47f75ad76cd
Navod <54998118+navodveduth@users.noreply.github.com>
2023-10-14 21:01:35 +0530

    staff schedule controller, routes

---
+++ server/controllers/staffScheduleController.js
@@ -0,0 +1,74 @@
+import asyncHandler from 'express-async-handler';
+import StaffSchedule from '../models/staffScheduleModel.js';
+
+export const createStaffSchedule = asyncHandler(async (req, res) => {
+  const { staff, date, shiftStart, shiftEnd } = req.body;
+  try {
+    const staffSchedule = new StaffSchedule({
+      staff,
+      date,
+      shiftStart,
+      shiftEnd,
+    });
+    const createdStaffSchedule = await staffSchedule.save();
+    res.status(201).json(createdStaffSchedule);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export const getStaffSchedules = asyncHandler(async (req, res) => {
+  try {
+    const staffSchedules = await StaffSchedule.find({}).populate('staff');
+    res.json(staffSchedules);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export const getStaffScheduleById = asyncHandler(async (req, res) => {
+  try {
+    const staffSchedule = await StaffSchedule.findById(req.params.id).populate(
+      'staff'
+    );
+    res.json(staffSchedule);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export const updateStaffSchedule = asyncHandler(async (req, res) => {
+  const { staff, date, shiftStart, shiftEnd } = req.body;
+  try {
+    const staffSchedule = await StaffSchedule.findById(req.params.id);
+    if (staffSchedule) {
+      staffSchedule.staff = staff;
+      staffSchedule.date = date;
+      staffSchedule.shiftStart = shiftStart;
+      staffSchedule.shiftEnd = shiftEnd;
+
+      const updatedStaffSchedule = await staffSchedule.save();
+      res.json(updatedStaffSchedule);
+    } else {
+      res.status(404);
+      throw new Error('Staff schedule not found');
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export const deleteStaffSchedule = asyncHandler(async (req, res) => {
+  try {
+    const staffSchedule = await StaffSchedule.findById(req.params.id);
+    if (staffSchedule) {
+      await staffSchedule.remove();
+      res.json({ message: 'Staff schedule removed' });
+    } else {
+      res.status(404);
+      throw new Error('Staff schedule not found');
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
---
+++ server/models/staffScheduleModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const staffScheduleSchema = new mongoose.Schema(
+  {
+    staff: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    shiftStart: {
+      type: String,
+      required: true,
+    },
+    shiftEnd: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const StaffSchedule = mongoose.model('StaffSchedule', staffScheduleSchema);
+
+export default StaffSchedule;
