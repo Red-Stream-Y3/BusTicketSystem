3281a166a92094fa643ac6d932eb52029c871811
Navod <54998118+navodveduth@users.noreply.github.com>
2023-10-15 12:45:04 +0530

    bus route, bus stop controllers

--- server/controllers/busStopController.js
+++ server/controllers/busStopController.js
@@ -1,16 +1,52 @@
-import asyncHandler from "express-async-handler";
-import BusStop from "../models/busStopModel.js";
+import asyncHandler from 'express-async-handler';
+import BusStop from '../models/busStopModel.js';
 
 export const createBusStop = asyncHandler(async (req, res) => {
-    const { name, location } = req.body;
-    try {
-        const busStop = new BusStop({
-            name,
-            location,
-        });
-        const createdBusStop = await busStop.save();
-        res.status(201).json(createdBusStop);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  const { name, location } = req.body;
+  const busStop = new BusStop({
+    name,
+    location,
+  });
+  const createdBusStop = await busStop.save();
+  res.status(201).json(createdBusStop);
+});
+
+export const getBusStops = asyncHandler(async (req, res) => {
+  const busStops = await BusStop.find({});
+  res.json(busStops);
+});
+
+export const getBusStopById = asyncHandler(async (req, res) => {
+  const busStop = await BusStop.findById(req.params.id);
+  if (busStop) {
+    res.json(busStop);
+  } else {
+    res.status(404);
+    throw new Error('Bus stop not found');
+  }
+});
+
+export const updateBusStop = asyncHandler(async (req, res) => {
+  const { name, location } = req.body;
+  const busStop = await BusStop.findById(req.params.id);
+  if (busStop) {
+    busStop.name = name;
+    busStop.location = location;
+    const updatedBusStop = await busStop.save();
+    res.json(updatedBusStop);
+  } else {
+    res.status(404);
+    throw new Error('Bus stop not found');
+  }
+});
+
+export const deleteBusStop = asyncHandler(async (req, res) => {
+  const busStop = await BusStop.findById(req.params.id);
+  if (busStop) {
+    await busStop.remove();
+    res.json({ message: 'Bus stop removed' });
+  } else {
+    res.status(404);
+    throw new Error('Bus stop not found');
+  }
 });
