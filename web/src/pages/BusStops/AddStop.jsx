0684227dbca1c1617ac11895ed9ba7589d0c3dff
Navod <54998118+navodveduth@users.noreply.github.com>
2023-10-15 12:48:47 +0530

    bus stops, staff schedule service

---
+++ web/src/pages/BusStops/AddStop.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { PageHeader } from '../../components';
+import { createBusStop } from '../../services/busStopService';
+
+const AddStop = () => {};
+
+export default AddStop;
